575ad38ec1a2b05c1057ca35bf4dc9cfdc8b6b19
Mina <minaemil329@gmail.com>
2023-08-17 14:53:18 +0300

    add home_repo and failure class

--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -1,4 +1,50 @@
-abstract class Failure{
+import 'package:dio/dio.dart';
 
-} 
-class ServerFailure extends Failure{}
+abstract class Failure {
+  final String errMessage;
+
+  Failure(this.errMessage);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.errMessage);
+
+  factory ServerFailure.fromDioException(DioException dioException) {
+    switch (dioException.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure("Connection timeout with ApiServer");
+      case DioExceptionType.sendTimeout:
+        return ServerFailure("Send timeout with ApiServer");
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure("Receive timeout with ApiServer");
+      case DioExceptionType.badCertificate:
+        return ServerFailure("Bad Server Certificate");
+      case DioExceptionType.badResponse:
+        ServerFailure.fromResponse(dioException.response!.statusCode, dioException.response!);
+      case DioExceptionType.cancel:
+        return ServerFailure("Request to ApiServer was canceled");
+      case DioExceptionType.connectionError:
+        return ServerFailure("No Internet Connection");
+      case DioExceptionType.unknown:
+        if (dioException.message!.contains('SocketException')) {
+          return ServerFailure('No Internet Connection');
+        }
+        return ServerFailure('Unexpected Error, Please try again!');
+      default:
+        return ServerFailure("Opps There was an Error, Please try again");
+    }
+    return ServerFailure("Opps There was an Error, Please try again");
+  }
+
+   factory ServerFailure.fromResponse(int? statusCode, dynamic response) {
+    if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(response['error']['message']);
+    } else if (statusCode == 404) {
+      return ServerFailure('Your request not found, Please try later!');
+    } else if (statusCode == 500) {
+      return ServerFailure('Internal Server error, Please try later');
+    } else {
+      return ServerFailure('Opps There was an Error, Please try again');
+    }
+  }
+}
--- lib/features/home/data/models/repos/home_repo_impl.dart
+++ lib/features/home/data/models/repos/home_repo_impl.dart
@@ -3,6 +3,7 @@ import 'package:bookly/core/utils/api_service.dart';
 import 'package:bookly/features/home/data/models/book_model/book_model/book_model.dart';
 import 'package:bookly/features/home/data/models/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl extends HomeRepo {
   final ApiService apiService;
@@ -12,19 +13,31 @@ class HomeRepoImpl extends HomeRepo {
   Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() async {
     try {
       var data =
-          await apiService.get(endPoint: "volumes?q=subject:computer science");
+          await apiService.get(endPoint: "volumes?Sorting=newest &q=subject:computer science");
       List<BookModel> books = [];
       for (var item in data["items"]) {
         books.add(BookModel.fromJson(item));
       }
       return right(books);
     } catch (e) {
-      return left(ServerFailure());
+      if (e is DioException) {
+        return left(
+          ServerFailure.fromDioException(
+            e,
+          ), 
+        );
+      }
+      return left(
+        ServerFailure(
+          e.toString(),
+        ),
+      );
     }
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async{try {
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
+    try {
       var data =
           await apiService.get(endPoint: "volumes?q=subject:computer science");
       List<BookModel> books = [];
@@ -33,6 +46,18 @@ class HomeRepoImpl extends HomeRepo {
       }
       return right(books);
     } catch (e) {
-      return left(ServerFailure());
-    }}
+      if (e is DioException) {
+        return left(
+          ServerFailure.fromDioException(
+            e,
+          ), 
+        );
+      }
+      return left(
+        ServerFailure(
+          e.toString(),
+        ),
+      );
+    }
+  }
 }
